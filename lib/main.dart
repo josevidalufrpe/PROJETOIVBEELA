9ca01e43d785a5b6a810b2a9afba543772085173
Max <marconemax42@gmail.com>
2020-03-24 14:57:51 -0300

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+import 'models/item.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+   
+        primarySwatch: Colors.yellow,
+      ),
+      home: HomePage(),
+      
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+
+  var items = new List<Item>();
+  
+  HomePage(){
+
+    items = [];
+    items.add(Item(title:"Item 1",done:false));
+    items.add(Item(title:"Item 2",done:true));
+    items.add(Item(title:"Item 3",done:false));
+
+
+  }
+
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("BeeLa App"),
+
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext context, int index){
+            
+            final item = widget.items[index];
+            
+            return CheckboxListTile(
+              
+              title: Text(item.title),
+              key: Key(item.title),
+              value: item.done,
+
+              onChanged: (value){
+
+                setState(() {
+                 
+                 item.done = value;
+   
+                });
+                
+               
+              },
+            
+            
+            );
+
+        },
+
+
+      
+      )
+
+
+    );
+  }
+}
+
