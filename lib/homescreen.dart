119fccdd0f6afb882a16371b9a403f7f46b42cf5
Acadi <ac.adi@hotmail.com>
2020-04-28 18:09:49 -0300

    atualização design

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
@@ -5,20 +6,17 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as JSON;
 import 'dart:async';
 
-
 class HomePage extends StatefulWidget {
   @override
   HomePageState createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
-
   bool _isLoggedIn = false;
   Map userProfile;
   final facebookLogin = FacebookLogin();
 
-
-  _loginWithFB() async{
+  _loginWithFB() async {
     final result = await facebookLogin.logInWithReadPermissions(['email']);
 
     switch (result.status) {
@@ -35,90 +33,115 @@ class HomePageState extends State<HomePage> {
         break;
 
       case FacebookLoginStatus.cancelledByUser:
-        setState(() => _isLoggedIn = false );
+        setState(() => _isLoggedIn = false);
         break;
       case FacebookLoginStatus.error:
-        setState(() => _isLoggedIn = false );
+        setState(() => _isLoggedIn = false);
         break;
     }
-
   }
 
-  _logout(){
-  facebookLogin.logOut();
-  setState(() {
-  _isLoggedIn = false;
-  });
+  _logout() {
+    facebookLogin.logOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
   }
 
-    @override
-    Widget build(BuildContext context) {
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarBrightness: Brightness.light
-      ));
-      // TODO: implement build
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          /*backgroundColor: Colors.lightGreen[400],*/
-          body: Container(
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.light));
+    // TODO: implement build
+    return Scaffold(
+        //debugShowCheckedModeBanner: false,
+        //home: Scaffold(
+        /*backgroundColor: Colors.lightGreen[400],*/
+        body: Container(
             decoration: BoxDecoration(color: Color(0xff95d865)),
             padding: EdgeInsets.only(
-                top: 60,
-                left: 40,
-                right: 40,
+              top: 60,
+              left: 40,
+              right: 40,
             ),
             child: Center(
-              child:SingleChildScrollView(
-
-
-
-
+              child: SingleChildScrollView(
                 child: _isLoggedIn
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                            padding: EdgeInsets.only(bottom: 32),
-                            child: Image.network(
-                              userProfile["picture"]["data"]["url"],
-                              height: 50.0,
-                              width: 50.0,
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 70,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                                image: NetworkImage(
+                                  userProfile["picture"]["data"]["url"],
+                                ),
+                              ),
                             ),
                           ),
-                          Text(userProfile["name"]),
-                          OutlineButton(
-                            child: Text("Logout"),
-                            onPressed: (){
+                          SizedBox(
+                            width: 20,
+                            height: 20,
+
+                            //Padding(
+                            //padding: EdgeInsets.only(bottom: 32),
+                          ),
+                          Text(
+                            userProfile["name"],
+                            style: TextStyle(fontWeight: FontWeight.w800),
+                          ),
+                          RaisedButton(
+                            color: Colors.blue[800],
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(39)),
+                            child: Text(
+                              "Sair",
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                            onPressed: () {
                               _logout();
-                              },
-                            )
+                            },
+                          )
                         ],
-                    )
+                      )
+                    : Padding(
+                        padding: EdgeInsets.only(),
+                        child: Row(children: <Widget>[
+                          Column(children: <Widget>[
+                            SizedBox(
+                              width: 80,
+                              height: 100,
+                              child: Image.asset("images/facebook.png"),
+                            ),
+                            RaisedButton(
+                              color: Colors.blue[800],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(39)),
+                              child: Text(
+                                "Login com o Facebook",
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w800),
+                              ),
+                              onPressed: () {
+                                _loginWithFB();
+                              },
+                            ),
+                          ]),
 
-
-                  : RaisedButton(
-                      color: Colors.blue[800],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(39)
+                        ]),
                       ),
-                      child: Text(
-                          "Login com o Facebook",
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800
-                      ),),
-                      onPressed: () {
-                        _loginWithFB();
-                      },
-                  ),
-                ),
               ),
-            ),
-          ),
-
-        );
-    }
+              //),
+            )));
   }
+}
